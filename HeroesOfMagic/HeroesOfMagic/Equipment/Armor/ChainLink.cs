1220ee65f280adcfc6d8d4f64538fe93f1a7064b
Nick Pawsey <pawsey18@gmail.com>
2021-08-07 16:07:47 -0300

    Knight characters class almost done

---
+++ HeroesOfMagic/HeroesOfMagic/Equipment/Armor/ChainLink.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroesOfMagic
+{
+    public class ChainLink
+    {
+        private readonly int health;
+        
+        public int Health
+        {
+            get
+            {
+                return this.health;
+            }
+          
+        }
+    }
+}
